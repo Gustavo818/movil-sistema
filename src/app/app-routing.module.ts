455938aa37ee2cde70d7fb09a03d2559a7f610e9
Gustavo818 <gustavoyancha@gmail.com>
2024-03-30 21:43:15 -0500

    Revert "update30-01"
    This reverts commit b9481dce2c8c5947e100e3f09ca85210cefd570a.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { LoginPageModule } from './seguridad/login/login.module';
 
 const routes: Routes = [
   {
-    path: 'login',
-    loadChildren: () => import('./seguridad/login/login.module').then( m => m.LoginPageModule)
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./seguridad/login/login.module').then( m => m.LoginPageModule)
   },
 ];
 
